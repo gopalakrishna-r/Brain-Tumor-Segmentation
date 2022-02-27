1bd44ac0dd6a2f58afbcc4bd53b34c38a7749f10
Gopalakrishna Ramachandra <3163730+gopalakrishna-r@users.noreply.github.com>
2022-02-28 03:55:26 +0530

    added tensor pack implementations and sequence processing

--- model.py
+++ model.py
@@ -1,170 +1,143 @@
+from tensorpack.callbacks import ModelSaver, InferenceRunner, ScheduledHyperParamSetter, ScalarStats, ClassificationError
+from tensorpack.train import launch_train_with_config, SimpleTrainer
+from tensorpack.train.config import TrainConfig
+from tensorpack.dataflow import dataset, imgaug, BatchData, AugmentImageComponent
+from tensorpack.utils import logger
+from tensorpack.graph_builder.model_desc import ModelDesc
+from tensorpack.tfutils.sessinit import SmartInit
+from tensorpack.tfutils.argscope import argscope
+from tensorpack.models import (AvgPooling, BatchNorm, Conv2D, GlobalAvgPooling, FullyConnected)
+from tensorpack.tfutils.common import get_global_step_var
+from tensorpack.tfutils.summary import add_moving_summary, add_param_summary
+from unicodedata import name
+
 import tensorflow.keras.backend as K
-from tensorflow.keras.models import Model
 from tensorflow.keras import Input
-from tensorflow.keras.layers import Conv2D, PReLU, UpSampling2D, concatenate , Reshape, Dense, Permute, MaxPool2D
-from tensorflow.keras.layers import GlobalAveragePooling2D, Activation, add, GaussianNoise, BatchNormalization, multiply
+from tensorflow.keras.layers import (Activation,
+                                     concatenate, 
+                                     GaussianNoise, GlobalAveragePooling2D,
+                                     MaxPool2D, Permute, PReLU, Reshape,
+                                     UpSampling2D, add, multiply)
+from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import SGD
+
 from loss import custom_loss
+
 K.set_image_data_format("channels_last")
+import tensorflow as tf
 
 
-
-def unet_model(input_shape, modified_unet=True, learning_rate=0.01, start_channel=64, 
-               number_of_levels=3, inc_rate=2, output_channels=4, saved_model_dir=None):
-    """
-    Builds UNet model
+def unet_model(input_shape, modified_unet = True, learning_rate = 0.01, start_channel = 64, 
+               number_of_levels = 3, inc_rate = 2, output_channels = 4, saved_model_dir = None):
     
-    Parameters
-    ----------
-    input_shape : tuple
-        Shape of the input data (height, width, channel)
-    modified_unet : bool
-        Whether to use modified UNet or the original UNet
-    learning_rate : float
-        Learning rate for the model. The default is 0.01.
-    start_channel : int
-        Number of channels of the first conv. The default is 64.
-    number_of_levels : int
-        The depth size of the U-structure. The default is 3.
-    inc_rate : int
-        Rate at which the conv channels will increase. The default is 2.
-    output_channels : int
-        The number of output layer channels. The default is 4
-    saved_model_dir : str
-        If spesified, the model weights will be loaded from this path. The default is None.
+    input_layer = Input(shape = input_shape, name = "input_layer")
+    name = 'unet'
+    with argscope([Conv2D], padding='same' ):
+            if modified_unet:
+                x = GaussianNoise(0.01, name='GaussianNoise')(input_layer)
+                x = Conv2D(f'{name}_conv_1.0', (x),  filters=64, kernel_size=2)
+                x = level_block_modified('modified_lvl_blk', x, start_channel, number_of_levels, inc_rate)
+                x = BatchNorm(f'{name}_BatchNorm', (x),  axis=3)
+                x = PReLU(shared_axes=[1, 2])(x)
+            else:
+                x = level_block('lvl_blk', x, input_layer, start_channel,number_of_levels, inc_rate)
 
-    Returns
-    -------
-    model : keras.model
-        The created keras model with respect to the input parameters
-
-    """
-
-        
-    input_layer = Input(shape=input_shape, name='the_input_layer')
-
-    if modified_unet:
-        x = GaussianNoise(0.01, name='Gaussian_Noise')(input_layer)
-        x = Conv2D(64, 2, padding='same')(x)
-        x = level_block_modified(x, start_channel, number_of_levels, inc_rate)
-        x = BatchNormalization(axis = -1)(x)
-        x = PReLU(shared_axes=[1, 2])(x)
-    else: 
-        x = level_block(input_layer, start_channel, number_of_levels, inc_rate)
-
-    x            = Conv2D(output_channels, 1, padding='same')(x)
+    x = Conv2D(f'{name}_conv_2.0' ,(x), output_channels, 1)
     output_layer = Activation('softmax')(x)
-    
-    model        = Model(inputs = input_layer, outputs = output_layer)
-
+        
+    model = Model(input_layer, output_layer)
+        
     if modified_unet:
-        print("The modified UNet was built!")
+        print("Using modified UNet")
     else:
-        print("The original UNet was built!")
-
+        print("Using standard UNet")
+    
     if saved_model_dir:
         model.load_weights(saved_model_dir)
-        print("the model weights were successfully loaded!")
-            
-    sgd = SGD(lr=learning_rate, momentum=0.9, decay=0)
-    model.compile(optimizer=sgd, loss=custom_loss)
+        print(f"Loaded weights from {saved_model_dir}")
+    
+    sgd = SGD(lr = learning_rate, momentum=0.9, decay=0.0)
+    model.compile(optimizer=sgd, loss=custom_loss, metrics=['accuracy'])
     
     return model
 
 
-def se_block(x, ratio=16):
-    
-    """
-    creates a squeeze and excitation block
-    https://arxiv.org/abs/1709.01507
-    
-    Parameters
-    ----------
-    x : tensor
-        Input keras tensor
-    ratio : int
-        The reduction ratio. The default is 16.
-
-    Returns
-    -------
-    x : tensor
-        A keras tensor
-    """
- 
-
-    channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+def se_block(name, x, ratio = 16):
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
     filters = x.shape[channel_axis]
     se_shape = (1, 1, filters)
-
-    se = GlobalAveragePooling2D()(x)
-    se = Reshape(se_shape)(se)
-    se = Dense(filters // ratio, activation='relu', kernel_initializer='he_normal', use_bias=False)(se)
-    se = Dense(filters, activation='sigmoid', kernel_initializer='he_normal', use_bias=False)(se)
-
-    if K.image_data_format() == 'channels_first':
-        se = Permute((3, 1, 2))(se)
-
-    x = multiply([x, se])
+    with argscope([FullyConnected], kernel_initializer='he_normal', kernel_regularizer=tf.keras.regularizers.l2(0.1)):
+            se = GlobalAveragePooling2D()(x)
+            se = Reshape(se_shape)(se)
+            se = FullyConnected(f'{name}_FCN_1.0', (se), filters // ratio, activation=tf.nn.relu)
+            se = FullyConnected(f'{name}_FCN_2.0', (se), filters, activation=tf.nn.sigmoid)
+            
+            if K.image_data_format() == 'channels_first':
+                se = Permute((3, 1, 2))(se)
+            x = multiply([x, se])
     return x
 
+    
 
-def level_block(x, dim, level, inc):
+def level_block(name, x, dim, level, inc):
     
     if level > 0:
-        m = conv_layers(x, dim)
+        m = conv_layers(f'{name}_{level}_conv_layer_m', x, dim)
+
         x = MaxPool2D(pool_size=(2, 2))(m)
-        x = level_block(x,int(inc*dim), level-1, inc)
+        x = level_block(f'{name}_{level - 1}_res1.0', x, int(dim * inc), level - 1, inc)
         x = UpSampling2D(size=(2, 2))(x)
-        x = Conv2D(dim, 2, padding='same')(x)
-        m = concatenate([m,x])
-        x = conv_layers(m, dim)
+        x = Conv2D((x), filters=dim, kernel_siz=2, padding='same')
+
+        m = concatenate([x, m])
+        x = conv_layers(f'{name}_{level}_conv_layer_x', m, dim)
     else:
-        x = conv_layers(x, dim)
+        x = conv_layers(f'{name}_{level}_conv_layer_x', x, dim)
     return x
 
 
-def level_block_modified(x, dim, level, inc):
+
+def level_block_modified(name, x, dim, level, inc):
+    with argscope([Conv2D], kernel_size=2,  padding='same'):
+            if level > 0:
+                m = res_block(f'{level}_res1.0', x, dim, encoder_path=True)
+                x = Conv2D(f'{name}_conv1.0', m, filters=int(inc * dim), stride=2)
+                x = level_block_modified(f'{level - 1}', x, int(dim * inc), level - 1, inc)
+
+                x = UpSampling2D(size=(2, 2))(x)
+                x = Conv2D(f'{name}_conv2.0', x, filters=dim)
+
+                m = concatenate([x, m])
+                m = se_block(f'{level}_seblock2.0', m, 8)
+                x = res_block(f'{level}_res2.0', m, dim, encoder_path=False)
+            else:
+                x = res_block(f'{level}_res1.0', x, dim, encoder_path=True)
+            return x   
+      
+def conv_layers(name, x, dim):
+        with argscope([Conv2D], filters=dim, kernel_size=3,  padding='same'):
+            with argscope([Activation], Activation='relu'):
+                x = Conv2D(f'{name}_conv_1.0', x)
+                x = Activation()(x)
+
+                x = Conv2D(f'{name}_conv_2.0', x)
+                x = Activation()(x)
+                return x
+
+
+def res_block(name, x, dim, encoder_path = True):
+        with argscope([Conv2D], filters=dim, kernel_size=3, padding='same'):
+                with argscope([BatchNorm], axis= 3):
+                    m = BatchNorm(f'{name}_BatchNorm_1.0', (x))
+                    m = PReLU(shared_axes=[1, 2])(m)
+                    m = Conv2D(f'{name}_conv_1.0', (m))
+
+                    m = BatchNorm(f'{name}_BatchNorm_2.0', (x))
+                    m = PReLU(shared_axes=[1, 2])(m)
+                    m = Conv2D(f'{name}_conv_2.0', (m))
+
+                    if not encoder_path:
+                        x = Conv2D(f'{name}_conv_encoder', x,  kernel_size=1)
+                    x = add([x, m])
+                    return x
     
-    if level > 0:
-        m = res_block(x, dim, encoder_path=True)##########
-        x = Conv2D(int(inc*dim), 2, strides=2, padding='same')(m)
-        x = level_block_modified(x, int(inc*dim), level-1, inc)
-
-        x = UpSampling2D(size=(2, 2))(x)
-        x = Conv2D(dim, 2, padding='same')(x)
-
-        m = concatenate([m,x])
-        m = se_block(m, 8)
-        x = res_block(m, dim, encoder_path=False)
-    else:
-        x = res_block(x, dim, encoder_path=True) #############
-    return x
-
-
-def conv_layers(x, dim):
-
-    x = Conv2D(dim, 3, padding='same')(x)
-    x = Activation("relu")(x)
-
-    x = Conv2D(dim, 3, padding='same')(x)
-    x = Activation("relu")(x)
-
-    return x
-
-def res_block(x, dim, encoder_path=True):
-
-    m = BatchNormalization(axis = -1)(x)
-    m = PReLU(shared_axes = [1, 2])(m)
-    m = Conv2D(dim, 3, padding='same')(m)
-
-    m = BatchNormalization(axis = -1)(m)
-    m = PReLU(shared_axes = [1, 2])(m)
-    m = Conv2D(dim, 3, padding='same')(m)
-
-    if encoder_path:
-        x = add([x, m])
-    else:
-        x = Conv2D(dim, 1, padding='same', use_bias=False)(x)
-        x = add([x,m])
-    return  x
-
--- prepare_data.py
+++ prepare_data.py
@@ -5,6 +5,7 @@ import nibabel as nib
 from tqdm import tqdm
 from glob import glob
 from config import cfg
+from functional import seq
 
 def read_brain(brain_dir, mode='train', x0=42, x1=194, y0=29, y1=221, z0=2, z1=146):
 
@@ -40,37 +41,39 @@ def read_brain(brain_dir, mode='train', x0=42, x1=194, y0=29, y1=221, z0=2, z1=1
     t1        = glob(os.path.join(brain_dir, '*_t1*.nii.gz'))
     t1ce      = glob(os.path.join(brain_dir, '*_t1ce*.nii.gz'))
     t2        = glob(os.path.join(brain_dir, '*_t2*.nii.gz'))
-    
+
     if mode=='train':
         gt             = glob( os.path.join(brain_dir, '*_seg*.nii.gz'))
         modalities_dir = [flair[0], t1[0], t1ce[0], t2[0], gt[0]]
-        
+
     elif mode=='validation':
         modalities_dir = [flair[0], t1[0], t1ce[0], t2[0]]   
-    
-    all_modalities = []    
-    for modality in modalities_dir:      
-        nifti_file   = nib.load(modality)
-        brain_numpy  = np.asarray(nifti_file.dataobj)    
-        all_modalities.append(brain_numpy)
-        
-    # all modalities have the same affine, so we take one of them (the last one in this case),
-    # affine is just saved for preparing the predicted nii.gz file in the future.       
-    brain_affine   = nifti_file.affine
+
+    all_modalities = []
+    (seq(modalities_dir).
+        map(nib.load).
+        map(lambda nifti_file: np.asarray(nifti_file.dataobj)).
+        for_each(all_modalities.append))
+
+    brain_affine = nib.load(modalities_dir[-1]).affine
     all_modalities = np.array(all_modalities)
     all_modalities = np.rint(all_modalities).astype(np.int16)
-    all_modalities = all_modalities[:, x0:x1, y0:y1, z0:z1]
-    # to fit keras channel last model
-    all_modalities = np.transpose(all_modalities) 
-    # tumor grade + name
-    brain_name     = os.path.basename(os.path.split(brain_dir)[0]) + '_' + os.path.basename(brain_dir) 
+    all_modalities = all_modalities[..., x0:x1, y0:y1, z0:z1]
+
+    all_modalities = np.transpose(all_modalities)
+
+    brain_name = (
+        f'{os.path.basename(os.path.split(brain_dir)[0])}_'
+        + os.path.basename(brain_dir)
+    )
+
 
     return all_modalities, brain_affine, brain_name
     
 
 
 def create_table(dataset_dir, table_data_shape, save_dir, crop_coordinates, data_channels, k_fold=None):
-    
+
     """
     Reads and saves all brain volumes into a single table file. 
     
@@ -100,67 +103,67 @@ def create_table(dataset_dir, table_data_shape, save_dir, crop_coordinates, data
     
     all_brains_dir = glob(dataset_dir)
     all_brains_dir.sort()
-    
-    hdf5_file    = tables.open_file(os.path.join(save_dir + 'data.hdf5'), mode='w')
+
+    hdf5_file = tables.open_file(f'{save_dir}data.hdf5', mode='w')
     filters      = tables.Filters(complevel=5, complib='blosc')
     data_shape   = tuple([0] + list(table_data_shape) + [data_channels])
     truth_shape  = tuple([0] + list(table_data_shape))
     affine_shape = tuple([0] + [4, 4])
-    
+
     data_storage   = hdf5_file.create_earray(hdf5_file.root, 'data', tables.UInt16Atom(), shape=data_shape,
                                                filters=filters, expectedrows=len(all_brains_dir))
     truth_storage  = hdf5_file.create_earray(hdf5_file.root, 'truth', tables.UInt8Atom(), shape=truth_shape,
                                                 filters=filters, expectedrows=len(all_brains_dir))
     affine_storage = hdf5_file.create_earray(hdf5_file.root, 'affine', tables.Float32Atom(), shape=affine_shape,
                                                 filters=filters, expectedrows=len(all_brains_dir))
-     
+
     brain_names = []
     for brain_dir in tqdm(all_brains_dir):
-        all_modalities, brain_affine, brain_name = read_brain(brain_dir, mode='train', **crop_coordinates)
+        all_modalities, brain_affine, brain_name = read_brain(brain_dir, mode='validation', **crop_coordinates)
         brain    = all_modalities[..., :4]
         gt       = all_modalities[..., -1]
-        
+
         # in BraTS 2017, 2018, 2019 there is no '3' label!
         gt[gt==4]  = 3    
         brain_names.append(brain_name)   
         data_storage.append(brain[np.newaxis,...])
         truth_storage.append(gt[np.newaxis,...])
         affine_storage.append(brain_affine[np.newaxis,...])
-        
+
     hdf5_file.create_array(hdf5_file.root, 'brain_names', obj=brain_names)
     hdf5_file.close()
-         
+
     if k_fold:
         validation_split = (1/k_fold) # this equal to 5-fold validation
         all_HGG_names = [i for i in brain_names if 'HGG' in i]
         all_LGG_names = [i for i in brain_names if 'LGG' in i]
-              
+
         np.random.seed(100)
         np.random.shuffle(all_HGG_names)
         np.random.shuffle(all_LGG_names)
-              
+
         HGG_val_size = int(validation_split * len(all_HGG_names))
         LGG_val_size = int(validation_split * len(all_LGG_names))
-        
+
         for fold in range(k_fold):
             chosen_HGG_val = all_HGG_names[fold*HGG_val_size:(fold+1)*HGG_val_size]
             chosen_LGG_val = all_LGG_names[fold*LGG_val_size:(fold+1)*LGG_val_size]
-        
+
             chosen_HGG_train = [i for i in all_HGG_names if i not in chosen_HGG_val]
             chosen_LGG_train = [i for i in all_LGG_names if i not in chosen_LGG_val]
-        
+
             # saving train_idx is enough
             train = chosen_HGG_train + chosen_LGG_train    
             train_idx = [brain_names.index(i) for i in train]    
             train_idx.sort()
-        
-            np.save(os.path.join(save_dir, 'fold{}_idx.npy'.format(fold)), train_idx)
+
+            np.save(f'{save_dir}/fold{fold}_idx.npy', train_idx)
     
    
     
 if __name__ == '__main__':
       
-    create_table(cfg['data_dir'], cfg['table_data_shape'], cfg['save_data_dir'], 
+    create_table(cfg['data_dir'], cfg['table_data_shape'], cfg['save_data_dir'],
                  cfg['crop_coord'], cfg['data_channels'], cfg['k_fold'])
     
 
